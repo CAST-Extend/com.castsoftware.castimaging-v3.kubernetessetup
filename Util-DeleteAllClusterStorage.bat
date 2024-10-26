da16368f5db315cdef7166c3fb53627a897621da
joseartero <j.artero@castsoftware.com>
2024-10-26 10:54:14 +0200

    JAR

--- Util-DeleteAllClusterStorage.bat
+++ Util-DeleteAllClusterStorage.bat
@@ -19,7 +19,7 @@ pause
 REM Retrieve and delete all Persistent Volumes (PVs)
 for /f "tokens=*" %%p in ('kubectl get pv --no-headers -o "custom-columns=NAME:.metadata.name" ^|findstr "^pvc-"') do (
     echo %%p
-    kubectl delete pv -n %NAMESPACE% %%p
+    kubectl delete pv %%p
     )
 )
 echo Done.
