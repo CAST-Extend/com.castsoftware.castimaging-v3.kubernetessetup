b576a4554e70804ef40bda50cf547f0b9e78b6aa
joseartero <j.artero@castsoftware.com>
2024-10-26 09:38:16 +0200

    Update Util-DeleteAllPV.bat

--- Util-DeleteAllPV.bat
+++ Util-DeleteAllPV.bat
@@ -1,9 +1,25 @@
 @echo off
-REM Retrieve all Persistent Volumes (PVs) with names starting with "pvc-"
-for /f "tokens=*" %%p in ('kubectl get pv --no-headers -o "custom-columns=NAME:.metadata.name"') do (
+set NAMESPACE=castimaging-v3
+
+echo ABOUT TO DELETE ALL PVCs IN %NAMESPACE%, ARE YOU SURE?
+pause
+echo PLEASE CONFIRM DELETION.
+pause     
+REM Retrieve and delete all Persistent Volumes (PVCs)
+for /f "tokens=*" %%p in ('kubectl get pvc -n %NAMESPACE% --no-headers -o "custom-columns=NAME:.metadata.name"') do (
     echo %%p
-    kubectl delete pv %%p
+    kubectl delete pvc -n %NAMESPACE% %%p
     )
 )
 
+echo ABOUT TO DELETE ALL PVs IN THE CLUSTER, ARE YOU SURE?
+pause
+echo PLEASE CONFIRM DELETION.
+pause
+REM Retrieve and delete all Persistent Volumes (PVs)
+for /f "tokens=*" %%p in ('kubectl get pv --no-headers -o "custom-columns=NAME:.metadata.name" ^|findstr "^pvc-"') do (
+    echo %%p
+    kubectl delete pv -n %NAMESPACE% %%p
+    )
+)
 echo Done.
