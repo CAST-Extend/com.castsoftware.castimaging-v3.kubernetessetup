28260c70d8a058b8c38c80395b1a29c5aece0f35
joseartero <j.artero@castsoftware.com>
2025-09-15 16:44:04 +0200

    3.4.3

--- helm-install.sh
+++ helm-install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-helm install castimaging-v3 --create-namespace --namespace castimaging-v3 --set version=3.4.2 .
+helm install castimaging-v3 --create-namespace --namespace castimaging-v3 --set version=3.4.3 .
--- helm-upgrade.bat
+++ helm-upgrade.bat
@@ -1,1 +1,1 @@
-helm upgrade castimaging-v3 --namespace castimaging-v3 --set version=3.4.2 .
+helm upgrade castimaging-v3 --namespace castimaging-v3 --set version=3.4.3 .
