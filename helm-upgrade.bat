cebccae4630cf03270db27a8ce4fb6585f4d5f91
joseartero <j.artero@castsoftware.com>
2026-02-18 17:37:56 +0100

    3.5.5

--- helm-upgrade.bat
+++ helm-upgrade.bat
@@ -1,1 +1,1 @@
-helm upgrade castimaging-v3 --namespace castimaging-v3 --set version=3.5.4 .
+helm upgrade castimaging-v3 --namespace castimaging-v3 --set version=3.5.5 .
--- helm-upgrade.sh
+++ helm-upgrade.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-helm upgrade castimaging-v3 --namespace castimaging-v3 --set version=3.5.4 .
+helm upgrade castimaging-v3 --namespace castimaging-v3 --set version=3.5.5 .
